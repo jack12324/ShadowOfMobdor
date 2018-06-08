e97caff20fadd6040743a872de608d7eb0db6ff7
Jack <jacksporter24@gmail.com>
2018-06-08 16:28:39 -0500

    Saucy entity rendering in gui

--- src/main/java/com/jack12324/som/gui/GuiDESC.java
+++ src/main/java/com/jack12324/som/gui/GuiDESC.java
@@ -13,6 +13,8 @@ public class GuiDESC extends GuiScreen {
     EntitySoMZombie mob;
     int xSize = 176;
     int ySize = 166;
+    float oldMouseX;
+    float oldMouseY;
     private static final ResourceLocation BG_TEXTURE = new ResourceLocation(ShadowOfMobdor.MODID, "textures/gui/desc.png");
 
     public GuiDESC(Entity mob) {
@@ -21,18 +23,26 @@ public class GuiDESC extends GuiScreen {
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-        super.drawScreen(mouseX, mouseY, partialTicks);
 
         GlStateManager.color(1, 1, 1, 1);
         mc.getTextureManager().bindTexture(BG_TEXTURE);
         int x = (width - xSize) / 2;
         int y = (height - ySize) / 2;
         drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
+        drawEntityOnScreen(x + 25, y + 80, 30, (float) (x + 25) - this.oldMouseX,
+                        (float) (y + 80 - 50) - this.oldMouseY, mob);
 
         String name = mob.getName();
-        fontRenderer.drawString(name, (xSize - fontRenderer.getStringWidth(name)) / 2, y + 5, 0x404040); 
+        fontRenderer.drawString(name, x + ((xSize - fontRenderer.getStringWidth(name)) / 2), y + 5,
+                        0x404040);
 
-        drawEntityOnScreen(x, y, 1, mouseX, mouseY, mob);
 
+
+        super.drawScreen(mouseX, mouseY, partialTicks);
+
+        this.oldMouseX = (float) mouseX;
+        this.oldMouseY = (float) mouseY;
     }
+
+
 }
